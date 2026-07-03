910b490581dbe26b9046a631f12bdf2ea87eeab8
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2026-07-03 08:04:18 +0530

    FastZap svg

---
+++ src/components/svg/FastZap.tsx
@@ -0,0 +1,72 @@
+export default function FastZap() {
+  return (
+    <div>
+      <svg width="196" height="242" viewBox="0 0 196 242" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line opacity="0.52" x1="134.533" y1="44.0368" x2="102.251" y2="75.3407" stroke="url(#paint0_linear_2060_407)" stroke-linecap="round" />
+        <line opacity="0.46" x1="143.533" y1="92.0368" x2="111.251" y2="123.341" stroke="url(#paint1_linear_2060_407)" stroke-linecap="round" />
+        <g filter="url(#filter0_f_2060_407)">
+          <path d="M41.3474 117.187L111.951 41.6C115.162 38.1623 120.912 40.6579 120.593 45.3512L117.772 86.9737C117.634 89.0029 118.739 90.9131 120.567 91.8052L153.136 107.702C156.252 109.223 156.912 113.377 154.42 115.788L67.2478 200.149C63.403 203.869 57.1891 199.766 59.1007 194.769L77.8459 145.769C78.7973 143.282 77.6325 140.485 75.1967 139.409L42.9805 125.174C39.8328 123.783 38.9984 119.702 41.3474 117.187Z" fill="#BEA2FF" />
+        </g>
+        <g filter="url(#filter1_f_2060_407)">
+          <path d="M41.3474 117.187L111.951 41.6C115.162 38.1623 120.912 40.6579 120.593 45.3512L117.772 86.9737C117.634 89.0029 118.739 90.9131 120.567 91.8052L153.136 107.702C156.252 109.223 156.912 113.377 154.42 115.788L67.2478 200.149C63.403 203.869 57.1891 199.766 59.1007 194.769L77.8459 145.769C78.7973 143.282 77.6325 140.485 75.1967 139.409L42.9805 125.174C39.8328 123.783 38.9984 119.702 41.3474 117.187Z" fill="#8450FF" />
+        </g>
+        <path d="M42.0551 116.43L111.016 42.6014C114.324 39.0592 120.232 41.8249 119.631 46.6346L114.051 91.2759C113.756 93.6348 115.163 95.8752 117.416 96.6343L151.764 108.208C155.313 109.404 156.336 113.935 153.644 116.539L68.165 199.261C64.2753 203.025 58.0114 198.786 60.0597 193.776L81.4556 141.438C82.5742 138.702 81.0877 135.598 78.2548 134.754L44.2817 124.635C40.7233 123.575 39.5206 119.143 42.0551 116.43Z" fill="#FFFAFF" stroke="#B48DDF" />
+        <line opacity="0.83" x1="179.533" y1="83.0368" x2="147.251" y2="114.341" stroke="url(#paint2_linear_2060_407)" stroke-linecap="round" />
+        <line x1="71.544" y1="132.037" x2="54.2511" y2="149.33" stroke="url(#paint3_linear_2060_407)" stroke-linecap="round" />
+        <line x1="117.544" y1="138.037" x2="100.251" y2="155.33" stroke="url(#paint4_linear_2060_407)" stroke-linecap="round" />
+        <line opacity="0.49" x1="155.544" y1="129.037" x2="138.251" y2="146.33" stroke="url(#paint5_linear_2060_407)" stroke-linecap="round" />
+        <line opacity="0.64" x1="159.544" y1="60.0369" x2="144.251" y2="75.3298" stroke="url(#paint6_linear_2060_407)" stroke-linecap="round" />
+        <line opacity="0.9" x1="96.5668" y1="54.0365" x2="67.2507" y2="85.307" stroke="url(#paint7_linear_2060_407)" stroke-linecap="round" />
+        <line opacity="0.6" x1="27.5283" y1="105.037" x2="5.25094" y2="126.345" stroke="url(#paint8_linear_2060_407)" stroke-linecap="round" />
+        <defs>
+          <filter id="filter0_f_2060_407" x="0" y="0" width="195.944" height="241.588" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="20" result="effect1_foregroundBlur_2060_407" />
+          </filter>
+          <filter id="filter1_f_2060_407" x="36" y="36" width="123.944" height="169.588" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="2" result="effect1_foregroundBlur_2060_407" />
+          </filter>
+          <linearGradient id="paint0_linear_2060_407" x1="101.196" y1="74.9708" x2="134.196" y2="42.9708" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" stop-opacity="0" />
+            <stop offset="1" stop-color="#D6C4FF" />
+          </linearGradient>
+          <linearGradient id="paint1_linear_2060_407" x1="110.196" y1="122.971" x2="143.196" y2="90.9708" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" stop-opacity="0" />
+            <stop offset="1" stop-color="#D6C4FF" />
+          </linearGradient>
+          <linearGradient id="paint2_linear_2060_407" x1="146.196" y1="113.971" x2="179.196" y2="81.9708" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" stop-opacity="0" />
+            <stop offset="1" stop-color="#D6C4FF" />
+          </linearGradient>
+          <linearGradient id="paint3_linear_2060_407" x1="53.1905" y1="148.976" x2="71.1905" y2="130.976" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" stop-opacity="0" />
+            <stop offset="1" stop-color="#D6C4FF" />
+          </linearGradient>
+          <linearGradient id="paint4_linear_2060_407" x1="99.1905" y1="154.976" x2="117.19" y2="136.976" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" stop-opacity="0" />
+            <stop offset="1" stop-color="#D6C4FF" />
+          </linearGradient>
+          <linearGradient id="paint5_linear_2060_407" x1="137.19" y1="145.976" x2="155.19" y2="127.976" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" stop-opacity="0" />
+            <stop offset="1" stop-color="#D6C4FF" />
+          </linearGradient>
+          <linearGradient id="paint6_linear_2060_407" x1="143.19" y1="74.9762" x2="159.19" y2="58.9762" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" stop-opacity="0" />
+            <stop offset="1" stop-color="#D6C4FF" />
+          </linearGradient>
+          <linearGradient id="paint7_linear_2060_407" x1="66.1792" y1="84.9878" x2="96.1792" y2="52.9878" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" stop-opacity="0" />
+            <stop offset="1" stop-color="#D6C4FF" />
+          </linearGradient>
+          <linearGradient id="paint8_linear_2060_407" x1="4.19839" y1="125.968" x2="27.1984" y2="103.968" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" stop-opacity="0" />
+            <stop offset="1" stop-color="#D6C4FF" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  )
+}
