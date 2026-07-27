d9f31b6060b3bce0167c0f5f2e5aa46814a905ae
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2026-07-27 21:52:29 +0530

    CubicStructure3D component

---
+++ src/components/svg/CubicStructure3D.tsx
@@ -0,0 +1,194 @@
+export default function CubicStructure3D() {
+  return (
+    <div>
+      <svg width="501" height="504" viewBox="0 0 501 504" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="3d-cubic-structure">
+          <g id="glow">
+            <g id="Ellipse 1" filter="url(#filter0_f_2244_79)">
+              <ellipse cx="174" cy="374" rx="31" ry="30" fill="white" />
+            </g>
+            <g id="Ellipse 2" filter="url(#filter1_f_2244_79)">
+              <ellipse cx="131" cy="271" rx="31" ry="30" fill="white" />
+            </g>
+            <g id="Ellipse 3" filter="url(#filter2_f_2244_79)">
+              <ellipse cx="188" cy="150" rx="31" ry="30" fill="white" />
+            </g>
+            <g id="Ellipse 4" filter="url(#filter3_f_2244_79)">
+              <ellipse cx="296" cy="130" rx="31" ry="30" fill="white" />
+            </g>
+            <g id="Ellipse 5" filter="url(#filter4_f_2244_79)">
+              <ellipse cx="370" cy="254" rx="31" ry="30" fill="white" />
+            </g>
+            <g id="Ellipse 6" filter="url(#filter5_f_2244_79)">
+              <ellipse cx="316" cy="363" rx="31" ry="30" fill="white" />
+            </g>
+          </g>
+          <g id="connected-cube-structure">
+            <g id="connected-cube">
+              <rect id="Rectangle 1" width="64.3479" height="64.3479" transform="matrix(0.866025 -0.5 0.866025 0.5 194 128.174)" fill="url(#paint0_linear_2244_79)" />
+              <rect id="Rectangle 4" width="97.0427" height="64.3479" transform="matrix(0.866025 -0.5 0.866025 0.5 111 272.521)" fill="url(#paint1_linear_2244_79)" />
+              <rect id="Rectangle 12" width="97.0427" height="64.3479" transform="matrix(-0.866025 -0.5 -0.866025 0.5 388.768 274.521)" fill="url(#paint2_linear_2244_79)" />
+              <path id="Rectangle 9" d="M249 160.174L304.727 128V225.459L249 257.633V160.174Z" fill="url(#paint3_linear_2244_79)" />
+              <rect id="Rectangle 13" width="64.3479" height="97.4591" transform="matrix(-0.866025 -0.5 0 1 249.727 160.174)" fill="url(#paint4_linear_2244_79)" />
+              <rect id="Rectangle 5" width="96.9949" height="64.3479" transform="matrix(0.866025 -0.5 0 1 166 304.174)" fill="url(#paint5_linear_2244_79)" />
+              <rect id="Rectangle 10" width="98.1495" height="64.3479" transform="matrix(-0.866025 -0.5 0 1 333 306.497)" fill="url(#paint6_linear_2244_79)" />
+              <rect id="Rectangle 2" width="64.3479" height="64.3479" transform="matrix(0.866025 0.5 0 1 111 272)" fill="url(#paint7_linear_2244_79)" />
+              <rect id="Rectangle 11" width="64.3479" height="64.3479" transform="matrix(-0.866025 0.5 0 1 388.727 274)" fill="url(#paint8_linear_2244_79)" />
+            </g>
+            <g id="connected-cube_2" opacity="0.29">
+              <g id="Group 3">
+                <rect id="Rectangle 3" width="34.641" height="99.0329" transform="matrix(0.866025 -0.5 0 1 250 160.321)" fill="#D9D9D9" />
+                <rect id="Rectangle 10_2" width="36.1751" height="97.9789" transform="matrix(0.866025 0.5 0 1 219 143)" fill="#D9D9D9" />
+              </g>
+              <g id="Group 1">
+                <rect id="Rectangle 7" width="35.8117" height="94.9733" transform="matrix(0.866025 -0.5 0.866025 0.5 250 258.906)" fill="#D9D9D9" />
+                <rect id="Rectangle 6" width="95.3744" height="31.42" transform="matrix(0.866025 0.5 0 1 250 259.03)" fill="#D9D9D9" />
+              </g>
+              <g id="Group 2">
+                <rect id="Rectangle 7_2" width="35.8117" height="94.9733" transform="matrix(-0.866025 -0.5 -0.866025 0.5 249.263 256.906)" fill="#D9D9D9" />
+                <rect id="Rectangle 6_2" width="95.3744" height="31.42" transform="matrix(-0.866025 0.5 0 1 249.263 257.03)" fill="#D9D9D9" />
+              </g>
+            </g>
+          </g>
+          <g id="cube">
+            <rect id="Rectangle 1_2" width="64.3479" height="64.3479" transform="matrix(0.866025 -0.5 0.866025 0.5 111 176.174)" fill="#151515" />
+            <rect id="Rectangle 3_2" width="64.3479" height="64.3479" transform="matrix(0.866025 -0.5 0 1 166 208.174)" fill="#7E7E7E" fill-opacity="0.4" />
+            <rect id="Rectangle 2_2" width="64.3479" height="64.3479" transform="matrix(0.866025 0.5 0 1 111 176)" fill="#151515" />
+          </g>
+          <g id="cube_2">
+            <rect id="Rectangle 1_3" width="64.3479" height="64.3479" transform="matrix(0.866025 -0.5 0.866025 0.5 278 176.174)" fill="#151515" />
+            <rect id="Rectangle 3_3" width="64.3479" height="64.3479" transform="matrix(0.866025 -0.5 0 1 333 208.174)" fill="#151515" />
+            <rect id="Rectangle 2_3" width="64.3479" height="64.3479" transform="matrix(0.866025 0.5 0 1 278 176)" fill="#7E7E7E" fill-opacity="0.4" />
+          </g>
+          <g id="cube_3">
+            <rect id="Rectangle 1_4" width="64.3479" height="64.3479" transform="matrix(0.866025 -0.5 0.866025 0.5 193 321.174)" fill="#7E7E7E" fill-opacity="0.4" />
+            <rect id="Rectangle 3_4" width="64.3479" height="64.3479" transform="matrix(0.866025 -0.5 0 1 248 353.174)" fill="#151515" />
+            <rect id="Rectangle 2_4" width="64.3479" height="64.3479" transform="matrix(0.866025 0.5 0 1 193 321)" fill="#151515" />
+          </g>
+          <g id="glow_2">
+            <g id="Ellipse 1_2" filter="url(#filter6_f_2244_79)">
+              <ellipse cx="199" cy="330.5" rx="14" ry="13.5" fill="white" />
+            </g>
+            <g id="Ellipse 2_2" filter="url(#filter7_f_2244_79)">
+              <ellipse cx="157.5" cy="251" rx="15.5" ry="15" fill="white" />
+            </g>
+            <g id="Ellipse 3_2" filter="url(#filter8_f_2244_79)">
+              <ellipse cx="203.5" cy="173" rx="15.5" ry="15" fill="white" />
+            </g>
+            <g id="Ellipse 4_2" filter="url(#filter9_f_2244_79)">
+              <circle cx="294" cy="173" r="13" fill="white" />
+            </g>
+            <g id="Ellipse 5_2" filter="url(#filter10_f_2244_79)">
+              <circle cx="340.5" cy="257.5" r="13.5" fill="white" />
+            </g>
+            <g id="Ellipse 6_2" filter="url(#filter11_f_2244_79)">
+              <circle cx="297.5" cy="331.5" r="12.5" fill="white" />
+            </g>
+          </g>
+          <path id="Polygon 1" d="M250.5 192L307.225 224V288L250.5 320L193.775 288V224L250.5 192Z" fill="#151515" />
+        </g>
+        <defs>
+          <filter id="filter0_f_2244_79" x="43" y="244" width="262" height="260" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter1_f_2244_79" x="0" y="141" width="262" height="260" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter2_f_2244_79" x="57" y="20" width="262" height="260" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter3_f_2244_79" x="165" y="0" width="262" height="260" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter4_f_2244_79" x="239" y="124" width="262" height="260" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter5_f_2244_79" x="185" y="233" width="262" height="260" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter6_f_2244_79" x="135" y="267" width="128" height="127" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="25" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter7_f_2244_79" x="92" y="186" width="131" height="130" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="25" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter8_f_2244_79" x="138" y="108" width="131" height="130" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="25" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter9_f_2244_79" x="231" y="110" width="126" height="126" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="25" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter10_f_2244_79" x="277" y="194" width="127" height="127" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="25" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <filter id="filter11_f_2244_79" x="235" y="269" width="125" height="125" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="25" result="effect1_foregroundBlur_2244_79" />
+          </filter>
+          <linearGradient id="paint0_linear_2244_79" x1="-0.79446" y1="34.8581" x2="30.0815" y2="64.0596" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#555555" />
+            <stop offset="0.496347" stop-color="#393939" />
+            <stop offset="1" stop-color="#585858" />
+          </linearGradient>
+          <linearGradient id="paint1_linear_2244_79" x1="48.5213" y1="-3.16906e-07" x2="44.7028" y2="45.6236" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#6E6E6E" />
+            <stop offset="1" stop-color="#BCBCBC" />
+          </linearGradient>
+          <linearGradient id="paint2_linear_2244_79" x1="48.5213" y1="-3.16906e-07" x2="44.7028" y2="45.6236" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#6E6E6E" />
+            <stop offset="1" stop-color="#BCBCBC" />
+          </linearGradient>
+          <linearGradient id="paint3_linear_2244_79" x1="302.628" y1="186.152" x2="273.608" y2="188.981" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#737373" />
+            <stop offset="1" stop-color="#BCBCBC" />
+          </linearGradient>
+          <linearGradient id="paint4_linear_2244_79" x1="61.9248" y1="56.9405" x2="30.259" y2="60.3155" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#737373" />
+            <stop offset="1" stop-color="#BCBCBC" />
+          </linearGradient>
+          <linearGradient id="paint5_linear_2244_79" x1="28.8675" y1="19.7598" x2="31.2969" y2="66.4083" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#BCBCBC" />
+            <stop offset="1" stop-color="#626262" />
+          </linearGradient>
+          <linearGradient id="paint6_linear_2244_79" x1="29.2112" y1="19.7598" x2="31.6121" y2="66.4112" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#BCBCBC" />
+            <stop offset="1" stop-color="#626262" />
+          </linearGradient>
+          <linearGradient id="paint7_linear_2244_79" x1="-1.74429e-07" y1="33.5" x2="32.41" y2="68.2499" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#585858" />
+            <stop offset="0.496899" stop-color="#393939" />
+            <stop offset="1" stop-color="#555555" />
+          </linearGradient>
+          <linearGradient id="paint8_linear_2244_79" x1="-1.74429e-07" y1="33.5" x2="32.41" y2="68.2499" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#585858" />
+            <stop offset="0.496899" stop-color="#393939" />
+            <stop offset="1" stop-color="#555555" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  )
+}
